6eeba2b632ba2e8ab3683a8a77ff71e64654567b
Jacob <jacobhtnguyen@gmail.com>
2020-02-08 21:00:42 -0500

    created environment files

---
+++ env/default.ts
@@ -0,0 +1,9 @@
+import { ENV } from './env';
+
+export const env: ENV = {
+  DB_HOST: 'localhost',
+  DB_PORT: 5432,
+  DB_USERNAME: 'postgres',
+  DB_PASSWORD: 'test',
+  DB_DATABASE: 'test',
+};
---
+++ env/production.ts
@@ -0,0 +1,9 @@
+import { ENV } from './env';
+
+export const env: ENV = {
+  DB_HOST: 'production-host',
+  DB_PORT: 5432,
+  DB_USERNAME: 'production-user',
+  DB_PASSWORD: 'production-password',
+  DB_DATABASE: 'production-database',
+};
