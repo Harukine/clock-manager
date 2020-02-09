f07db4d65237560f79ea93ac2890290c54c84c1f
Jacob <jacobhtnguyen@gmail.com>
2020-02-09 16:38:50 -0500

    created auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { AUTH_REPOSITORY_TOKEN, USER_REPOSITORY_TOKEN } from '../../common/config/database.tokens.constants';
+import { Repository } from 'typeorm';
+import { AuthEntity } from './entities/auth.entity';
+import { AuthDto } from './dto/auth.dto';
+import { AuthResponseDto } from './dto/auth.response.dto';
+import { INPUT, OUTPUT, STATUS_CODE_RESPONSE } from './constants/auth.constants';
+import * as moment from 'moment';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(AUTH_REPOSITORY_TOKEN)
+    private readonly authRepository: Repository<AuthEntity>,
+    @Inject(USER_REPOSITORY_TOKEN)
+    private readonly userRepository: Repository<User>
+  ) {
+  }
+
+  async authIn(auth: AuthDto): Promise<AuthResponseDto> {
+    try {
+      const user = await this.saveTicketing({ ...auth, reader: INPUT });
+      return this.welcomeUser(user.name);
+    } catch (error) {
+      return { status: STATUS_CODE_RESPONSE.KO, msg: `Input Error ${error}` };
+    }
+  }
+
+  async authOut(auth: AuthDto): Promise<AuthResponseDto> {
+    try {
+      const user = await this.saveTicketing({...auth, reader: OUTPUT});
+      return this.byeUser(user.name);
+    } catch (error) {
+      return { status: STATUS_CODE_RESPONSE.KO, msg: `Output Error ${error}` };
+    }
+  }
+
+  private async saveTicketing(auth: AuthDto): Promise<User> {
+    const user = await this.userRepository.findOne({
+      where: {
+        key: auth.key,
+      },
+    });
+    if (!user) {
+      throw new Error();
+    }
+    await this.authRepository.save({
+      ...auth,
+      user,
+      timestamp: moment().unix(),
+    });
+    return user;
+  }
+
+  private welcomeUser(userName) {
+    return {
+      status: STATUS_CODE_RESPONSE.OK,
+      msg: `Input - ${userName}`,
+    };
+  }
+
+  private byeUser(userName) {
+    return {
+      status: STATUS_CODE_RESPONSE.OK,
+      msg: `Output - ${userName}`,
+    };
+  }
+}
